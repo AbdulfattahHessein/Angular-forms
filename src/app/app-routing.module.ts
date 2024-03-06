3fcb3859414751f769b038d9754b58488dab5319
Abdulfattah Hessein <abdulfattah.hessein@gmail.com>
2024-03-06 20:07:02 +0200

    add default routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { RegistrationFormComponent } from './components/registration-form/registration-form.component';
 
 const routes: Routes = [
-  // { path: '', redirectTo: '/registration', pathMatch: 'full' },
+  { path: '', redirectTo: '/registration', pathMatch: 'full' },
   { path: 'registration', component: RegistrationFormComponent },
+  { path: '**', redirectTo: '/registration', pathMatch: 'full' },
 ];
 
 @NgModule({
